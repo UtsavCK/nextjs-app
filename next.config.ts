4546eb25c99cc87c616c3aad048c563889f9f733
Utsav CK <utsavkayastha.000@gmail.com>
2025-04-26 21:17:30 +0545

    added loading page and wrapped dynamic components under suspense boundary
    fetching data in lower components instead of fetching data and passing it as props. skeleton page for components that require data fetching and time. group card components to avoid popping effect.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //improves performance but is experimental and not recommended for production
+  // experimental: {
+  //   ppr: 'incremental',
+  // }
 };
 
 export default nextConfig;
