df532d8208f7b2fb48de725ef9ffaaeafb09340d
Utsav CK <utsavkayastha.000@gmail.com>
2025-05-05 21:25:18 +0545

    Changed bcrypt to bcryptjs

--- auth.ts
+++ auth.ts
@@ -3,7 +3,7 @@ import { authConfig } from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
 import { z } from "zod";
 import type { User } from "@/app/lib/definitions";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { sql } from "@vercel/postgres";
 
 async function getUser(email: string): Promise<User | undefined> {
